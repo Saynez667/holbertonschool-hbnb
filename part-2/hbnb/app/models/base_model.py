b3cc8143681f1f010c484303a366945a262dd67e
Saynez667 <saynezironx@gmail.com>
2025-02-24 18:54:52 +0100

    i have change base model.py

--- part-2/hbnb/app/models/base_model.py
+++ part-2/hbnb/app/models/base_model.py
@@ -11,9 +11,21 @@ class BaseModel:
         """Update the updated_at timestamp whenever the object is modified"""
         self.updated_at = datetime.now()
 
+    def to_dict(self):
+        """Return a dictionary representation of the BaseModel instance"""
+        data = self.__dict__.copy()
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        data['__class__'] = self.__class__.__name__
+        return data
+
+    def __str__(self):
+        """Return a string representation of the BaseModel instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
     def update(self, data):
         """Update the attributes of the object based on the provided dictionary"""
         for key, value in data.items():
             if hasattr(self, key):
                 setattr(self, key, value)
-        self.save()  # Update the updated_at timestamp
+        self.save()
